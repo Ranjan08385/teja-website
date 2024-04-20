a8b53d87c8261fe47b49d0003d7f9860d73cbe75
Ranjan08385 <moger.ranjan@gmail.com>
2024-04-20 12:49:09 +0530

    new changes

---
+++ src/screens/home_page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/home_page.css";
+
+const HomePage = () => {
+  return (
+    <div className='main'><h1>Teja Moger</h1></div>
+  )
+}
+
+export default HomePage
